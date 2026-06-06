93d44727b9dcf4b02b23647cc8c619ac9e9756f7
hihihehe <setsuna01668298591@gmail.com>
2026-06-06 11:51:27 +0700

    Create h.py

---
+++ h.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://vinid.vn/hehehehihi"
+
+headers = {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkZha2UgVXNlciIsImlhdCI6MTcxNzU0NTYwMH0.fake_signature_1234567890",
+    "Content-Type": "application/json"
+}
+
+response = requests.get(url, headers=headers)
+
+print("Status:", response.status_code)
+print("Response:")
+print(response.text)
